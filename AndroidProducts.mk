2f1e055a858b804e28f2e2413b2c52442ad98032
Jprimero15 <jprimero155@gmail.com>
2022-05-12 05:46:19 +0000

    initial bringup
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_Infinix-X6812.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_Infinix-X6812-user \
+    omni_Infinix-X6812-userdebug \
+    omni_Infinix-X6812-eng
---
+++ omni_Infinix-X6812.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from Infinix-X6812 device
+$(call inherit-product, device/infinix/Infinix-X6812/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := Infinix-X6812
+PRODUCT_NAME := omni_Infinix-X6812
+PRODUCT_BRAND := Infinix
+PRODUCT_MODEL := Infinix X6812
+PRODUCT_MANUFACTURER := infinix
