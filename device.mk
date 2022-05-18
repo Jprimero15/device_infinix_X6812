6efb4a9c9cade81453f6c414df0fb37bcf13aa21
Jprimero15 <jprimero155@gmail.com>
2022-05-18 23:14:14 +0800

    X6812: Import infinix otacerts
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- device.mk
+++ device.mk
@@ -80,3 +80,7 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# OEM otacerts
+PRODUCT_EXTRA_RECOVERY_KEYS += \
+    $(LOCAL_PATH)/security/infinix
