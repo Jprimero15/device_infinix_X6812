fc513fa74f394c872cefdbc59b62a69bb994c470
Jprimero15 <jprimero155@gmail.com>
2022-05-18 23:55:17 +0800

    X6812: bootctrl: Fully remove UFS support
    * we only use emmc on this device
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- bootctrl/boot_region_control.cpp
+++ bootctrl/boot_region_control.cpp
@@ -40,8 +40,7 @@
 #include <android-base/logging.h>
 
 #if !defined(ARCH_X86)
-#include <mmc-mtk-ioctl.h>
-#include <ufs-mtk-ioctl.h>
+#include "mmc-mtk-ioctl.h"
 #include <sys/ioctl.h>
 #endif
 
@@ -121,34 +120,6 @@ static bool emmc_set_active_boot_part(int bootpart)
   return ret;
 }
 
-static bool ufs_set_active_boot_part(int boot)
-{
-    struct ufs_ioctl_query_data idata;
-    unsigned char buf[1];
-    int fd, ret = true;
-
-    fd = open("/dev/block/sdc", O_RDWR);
-    if (fd < 0) {
-        printf("%s: open device failed, err: %d\n", __func__, fd);
-        return false;
-    }
-
-    buf[0] = boot;           /* 1: BootLU A, 2: BootLU B */
-
-    idata.opcode = UPIU_QUERY_OPCODE_WRITE_ATTR;
-    idata.idn = QUERY_ATTR_IDN_BOOT_LUN_EN;
-    idata.idx = 0;
-    idata.buf_ptr = &buf[0];
-    idata.buf_byte = 1;
-
-    if (ioctl(fd, UFS_IOCTL_QUERY, &idata) < 0) {
-      LOG(ERROR) << "ufs_set boot_part fail";
-      ret = false;
-    }
-    close(fd);
-    return ret;
-}
-
 bool BootControlExt::SetBootRegionSlot(unsigned int slot) {
   int boot_part = 0;
 
@@ -165,9 +136,8 @@ bool BootControlExt::SetBootRegionSlot(unsigned int slot) {
   else
     boot_part = 1;
 
-  if (!ufs_set_active_boot_part(boot_part))
-    if (!emmc_set_active_boot_part(boot_part))
-      return false;
+  if (!emmc_set_active_boot_part(boot_part))
+	return false;
 
   return true;
 }
