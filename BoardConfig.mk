8d542efe3662f1739c7020b8f02997a8ecc05814
Jprimero15 <jprimero155@gmail.com>
2022-05-23 12:25:57 +0800

    X6812B: more changes for NFC variant
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,7 +43,6 @@ AB_OTA_UPDATER := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
-BOARD_KERNEL_CMDLINE += androidboot.force_normal_boot=1
 TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
 TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
 BOARD_BOOTIMG_HEADER_VERSION := 2
